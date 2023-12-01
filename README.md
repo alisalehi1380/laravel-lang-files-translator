1f0bb22003ef05dff56407c5d9fc1075edb856ed
Ali Salehi <alisalehi13800930@gmail.com>
2023-12-02 00:11:03 +0330

    Add(readme): file & detail to commands/translate

---
+++ README.md
@@ -0,0 +1,34 @@
+# Laravel Lang Files Translator
+
+![laravel_traslator-lang-files.jpeg](..%2Flaravel_traslator-lang-files.jpeg)
+[![Latest Stable Version](http://poser.pugx.org/alisalehi/laravel-lang-files-translator/v)](https://packagist.org/packages/alisalehi/laravel-lang-files-translator)
+[![Total Downloads](http://poser.pugx.org/alisalehi/laravel-lang-files-translator/downloads)](https://packagist.org/packages/alisalehi/laravel-lang-files-translator)
+[![License](http://poser.pugx.org/alisalehi/laravel-lang-files-translator/license)](https://packagist.org/packages/alisalehi/laravel-lang-files-translator)
+[![PHP Version Require](http://poser.pugx.org/alisalehi/laravel-lang-files-translator/require/php)](https://packagist.org/packages/alisalehi/laravel-lang-files-translator)
+***
+Laravel lang files translator is a package for Easiest way to translate lang files to other languages in PHP format in laravel. <br>
+
+# Requirements
+***
+
+- `PHP: >=8.0`
+- `stichoza/google-translate-php: ^5.1`
+- `Laravel Framework: 9.*|10.*`
+
+# Installation
+***
+```
+composer require alisalehi/laravel-lang-files-translator
+```
+
+# Usage
+First, you use trait in model.
+```
+
+```
+
+## Contributing
+This project exists thanks to all the people who
+contribute. [CONTRIBUTING](https://github.com/alisalehi/laravel-lang-files-translator/graphs/contributors)
+
+<a href="https://github.com/alisalehi/laravel-lang-files-translator/graphs/contributors"><img src="https://opencollective.com/laravel-lang-files-translator/contributors.svg?width=890&button=false" /></a>
--- src/Commands/Translate.php
+++ src/Commands/Translate.php
@@ -2,7 +2,7 @@
 
 namespace Alisalehi\LaravelLangFilesTranslator\Commands;
 
-use AliSalehi\LaravelLangFilesTranslator\Services\TranslateService;
+use Alisalehi\LaravelLangFilesTranslator\Services\TranslateService;
 use Illuminate\Console\Command;
 
 class Translate extends Command
@@ -14,18 +14,22 @@ class Translate extends Command
     public function handle(TranslateService $translateService)
     {
         $this->info('start translation. please wait...');
+        $this->info(PHP_EOL . 'The speed of translation of files depends on the speed of the Internet,');
+        $this->info('the number of file lines and the indentation of each key.');
+        $this->info('So please be patient until the translation of the files is finished.');
+        $this->info('Thankful');
         
         $translateService->to($this->argument('to'))->from($this->argument('from'))->translate();
         
-        $this->thanks();
+        $this->getOutput()->writeln(PHP_EOL . ' - Finished translation! (go to lang/' . $this->argument('to') . ' folder) ');
         
-        $this->getOutput()->writeln(' - Finished translation. (go to ' . $this->argument('to') . ' folder) ');
+        $this->thanks();
     }
     
     public function thanks(): void
     {
         $this->line(PHP_EOL . '<fg=blue>|-------------------------------------------------|</>');
-        $this->line('<fg=blue>|-----------     Star Me On Github     -----------|</>');
+        $this->line('<fg=blue>|-----------     <fg=yellow>Star Me On Github</>     -----------|</>');
         $this->line('<fg=blue>|-------------------------------------------------|</>');
         $this->line('<fg=blue>|  if you have found lang-files-translator useful |</>');
         $this->line('<fg=blue>|  Please consider giving it an star on github.   |</>');
